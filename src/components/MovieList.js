72ed6323e64e6a691025f80a49b5634dd8e9beb1
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-05-26 18:48:19 -0300

    Requisito 4 feito

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,8 +1,13 @@
 import React from 'react';
-// import movies from '../data';
+import MovieCard from './MovieCard';
 
 export default class MovieList extends React.Component {
   render() {
-    return <h1>Hello</h1>;
+    const { movies } = this.props;
+    return (
+    <div>
+      {movies.map((movie) => <MovieCard movie={movie} />)}
+    </div>
+    );
   }
 }
