833a6ee41c81e6fd91f3e16162f483d1bfebd0e8
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-05-26 18:54:26 -0300

    Requisito 4 correcao

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
 
 export default class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-    <div>
-      { movies.map((movie, index) => <MovieCard movie={ movie } key={ index } />) }
-    </div>
+      <div>
+        { movies.map((movie, index) => <MovieCard movie={ movie } key={ index } />) }
+      </div>
     );
   }
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object),
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
-
