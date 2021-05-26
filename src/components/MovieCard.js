3965f3b424331275caaca51161d030938955d4da
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-05-26 19:03:37 -0300

    Requisito 6 feito

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class MovieCard extends React.Component {
   render() {
     return <h1>Hello</h1>;
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    ratingname: PropTypes.number,
+  }).isRequired,
+
+};
