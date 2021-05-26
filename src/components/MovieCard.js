e61a69ef1956d55f15cdbc4b782b450f6096849e
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-05-26 19:09:35 -0300

    Requisito 9 feito

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,6 +8,7 @@ export default class MovieCard extends React.Component {
     <div>
       <img src={ imagePath }></img>
       <h4>{ title }</h4>
+      <h5>{ subtitle }</h5>
     </div>
     );
   }
