7459629036af93a62d806055e988926e4a9e6303
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-05-26 19:11:55 -0300

    Requisito 11 feito

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 
 export default class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, ratingname } = this.props.movie;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
     <div>
       <img src={ imagePath }></img>
       <h4>{ title }</h4>
       <h5>{ subtitle }</h5>
       <p>{ storyline }</p>
+      <Rating rating={ rating }/>
     </div>
     );
   }
@@ -21,6 +22,6 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
-    ratingname: PropTypes.number,
+    rating: PropTypes.number,
   }).isRequired,
 };
